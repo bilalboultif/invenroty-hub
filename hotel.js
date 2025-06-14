28e99c20643c23f3f7d1bc84769357cb007e13f9
bilalboultif <80066430+bilalboultif@users.noreply.github.com>
2025-06-14 02:14:09 -0400

    Update hotel.js

--- hotel.js
+++ hotel.js
@@ -1,8 +1,8 @@
   window.env = {
-        HOTELID1: '76575619-383e-44b3-9d9c-fd913afef0a1',
-        HOTELID2: '6844e2596e4bf672d629e738',
-        STRIPID: 'pk_test_51QiT4sE0C522oYCMhMZIAieuPJmjMyLZywCbn8w3DSdTTOl1JKwQrjr2fYoKncbK9SCayHVe5jriFmzPnmcexzPM00w5KgcTFA',
-        AUTH_API_URL: 'https://my-node-backend-fcdy.onrender.com'
+        HOTELID1: '8888888888888888888888888',
+        HOTELID2: '888888888888888888888888888',
+        STRIPID: '88888888888888888888888888888888888888888888',
+        AUTH_API_URL: 'https://8888888888888888888888888888'
     };
 
     
